24a28f0b0ee99d7f509dec4e868eb250a941131c
runar <git@runar.st>
2022-01-22 23:03:22 +0100

    Make code comply with the PEAR standard

--- hooks/Text_Parser.php
+++ hooks/Text_Parser.php
@@ -1,43 +1,46 @@
 //<?php
 
 /* To prevent PHP errors (extending class does not exist) revealing path */
-if ( !\defined( '\IPS\SUITE_UNIQUE_KEY' ) )
-{
-	exit;
+if (!\defined('\IPS\SUITE_UNIQUE_KEY')) {
+    exit;
 }
 
 class hook76 extends _HOOK_CLASS_
 {
-	/**
-	 * Get URL bases (without schema) that we'll allow iframes from
-	 *
-	 * @return array
-	 */
-	protected static function allowedIFrameBases()
-	{
-		$return = parent::allowedIFrameBases();
+    /**
+     * Get URL bases (without schema) that we'll allow iframes from
+     *
+     * @return array
+     */
+    protected static function allowedIFrameBases()
+    {
+        $return = parent::allowedIFrameBases();
 
-		$return = array_merge( $return, array(
-			'streamable.com/'
-		) );
+        return array_merge(
+            $return, array(
+                'streamable.com/',
+            ),
+        );
+    }
 
-		return $return;
-	}
+    /**
+     * Get OEmbed Services
+     *
+     * @see <a href="http://www.oembed.com">oEmbed</a>
+     *
+     * @return array
+     */
+    protected static function oembedServices()
+    {
+        $return = parent::oembedServices();
 
-	/**
-	 * Get OEmbed Services
-	 *
-	 * @see <a href="http://www.oembed.com">oEmbed</a>
-	 * @return array
-	 */
-	protected static function oembedServices()
-	{
-		$return = parent::oembedServices();
-
-		$return = array_merge( $return, array(
-			'streamable.com' => array( 'https://api.streamable.com/oembed.json', static::EMBED_VIDEO ),
-		) );
-
-		return $return;
-	}
+        return array_merge(
+            $return, array(
+                'streamable.com' => array(
+                    'https://api.streamable.com/oembed.json',
+                    static::EMBED_VIDEO,
+                ),
+            ),
+        );
+    }
 }
