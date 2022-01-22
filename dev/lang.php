c6ff5f59cce0dda16db4eea494e8b3757369b93e
runar <git@runar.st>
2022-01-22 23:17:29 +0100

    Release version 1.1.0 (#2)
    * Update language string to latest from Invision Community
    * Add version 1.1.0

--- dev/lang.php
+++ dev/lang.php
@@ -1,5 +1,5 @@
 <?php
 
 $lang = array(
-    'editor_embeds_desc' => "Certain content can be automatically embedded. For example, if a user posts a link to a video on YouTube or Vimeo the video can be embedded into the post. Changing this setting won't apply to existing content.<br>Links from the following sites are supported: College Humor, Coub, Dailymotion, Facebook, Flickr, Funny Or Die, Gfycat, Google Maps, Hulu, Instagram, ReverbNation, Screencast.com, SmugMug, SoundCloud, Spotify, Streamable, TED, Twitch, Twitter, Ustream, Vimeo, Vine, Wistia, YouTube, and internal links to content within the community.",
+    'editor_embeds_desc' => "Certain content can be automatically embedded. For example, if a user posts a link to a video on YouTube or Vimeo the video can be embedded into the post. Changing this setting won't apply to existing content.<br>Links from the following sites are supported: College Humor, Coub, Dailymotion, Facebook, Flickr, Funny Or Die, Gfycat, Google Maps, Hulu, Instagram, ReverbNation, Screencast.com, SmugMug, SoundCloud, Spotify, Streamable, TED, Twitter, Ustream, Vimeo, Vine, Wistia, YouTube, and internal links to content within the community.",
 );
